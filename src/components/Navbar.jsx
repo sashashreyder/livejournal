55be72dc1bed056144be8f65c8a19fb050485db2
sashashreyder <sashashreiderbr@gmail.com>
2025-02-25 18:13:49 -0300

    Font Awesome installed, icon for profile added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "/src/styles/Navbar.css"; 
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
-            <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>Profile</NavLink>
-            <NavLink to="/diary" className={({ isActive }) => isActive ? "active" : ""}>Diary</NavLink>
-            <NavLink to="/feed" className={({ isActive }) => isActive ? "active" : ""}>Feed</NavLink>
-            <NavLink to="/search" className={({ isActive }) => isActive ? "active" : ""}>Search</NavLink>
+            <div className="nav-links">
+                <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
+                <NavLink to="/diary" className={({ isActive }) => isActive ? "active" : ""}>Diary</NavLink>
+                <NavLink to="/feed" className={({ isActive }) => isActive ? "active" : ""}>Feed</NavLink>
+                <NavLink to="/search" className={({ isActive }) => isActive ? "active" : ""}>Search</NavLink>
+            </div>
+            
+            <NavLink to="/profile" className="profile-icon">
+                <FontAwesomeIcon icon={faUser} />
+            </NavLink>
         </nav>
     );
 }
 
 export default Navbar;
+
 
 
